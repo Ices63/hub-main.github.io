4eb3e96a68002040d7396a13b47a2e53627acb80
Brice Bayard <brice.bayard@gmail.com>
2024-12-06 12:39:39 +0100

    idea + getUrl

--- app/page_old.tsx
+++ app/page_old.tsx
@@ -1,26 +1,26 @@
-"use client"
-
-import { usePathname } from 'next/navigation'
 import Image from "next/image";
 
 export default function Home() {
-
-  var LinkHubOne = "blop";
-  const currentPathname = usePathname();
-  console.log(currentPathname);
-
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1>Hub-main</h1>
-        <p> Plusieurs outils sont en prévision de développement, vous pouvez y accéder depuis cette plateforme centrale : </p>
+        <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2"> <a href={`../${null}`}> Schématisations et notes des idées </a> </li>
-          <li className="mb-2"> <a href=""> Conception des architectures physiques et virtuelles </a> </li>
-          <li className="mb-2"> <a href=""> Coneption BDD </a> </li>
-          <li className="mb-2"> <a href=""> Maquettage des pages </a> </li>
-          <li className="mb-2"> <a href=""> Planning de Gantt des installations et développements </a> </li>
-          <li className="mb-2"> <a href=""> Gestion des tâches d'équipes </a> </li>
+          <li className="mb-2">
+            Get started by editing{" "}
+            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+              app/page.tsx
+            </code>
+            .
+          </li>
+          <li>Save and see your changes instantly.</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
